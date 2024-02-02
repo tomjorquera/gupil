d195105967c51136900fd2570bdd74ea44e30261
Tom Jorquera <tom@jorquera.net>
2024-02-02 22:40:10 +0100

    Add eli5 action

--- src/popup/gupop.js
+++ src/popup/gupop.js
@@ -1,18 +1,20 @@
+function call_to_action(action) {
+  chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      contentquery: action,
+    });
+  });
+}
+
 function listenForClicks() {
   document.addEventListener("click", (e) => {
     const optionName = e.target.textContent;
     switch (optionName) {
       case "Summarize":
-        chrome.tabs
-          .query({ active: true, currentWindow: true })
-          .then((tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {
-              contentquery: "summarize",
-            });
-          });
+        call_to_action("summarize");
         return;
       case "ELI5":
-        console.log("Selected ELI5");
+        call_to_action("eli5");
         return;
       case "Translate":
         console.log("Selected Translate");
