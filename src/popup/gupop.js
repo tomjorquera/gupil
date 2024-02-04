ca0bbe34a86f1e8aafdfcab193c7daf0a8775636
Tom Jorquera <tom@jorquera.net>
2024-02-04 22:58:07 +0100

    Make gupop available actions dynamic

--- src/popup/gupop.js
+++ src/popup/gupop.js
@@ -1,4 +1,5 @@
 function call_to_action(action) {
+  chrome.sidebarAction.open();
   chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {
       type: "contentquery",
@@ -8,30 +9,35 @@ function call_to_action(action) {
   });
 }
 
-function listenForClicks() {
-  document.addEventListener("click", (e) => {
-    chrome.sidebarAction.open();
-    const optionName = e.target.textContent;
-    switch (optionName) {
-      case "Summarize":
-        call_to_action("summarize");
-        return;
-      case "ELI5":
-        call_to_action("eli5");
-        return;
-      case "Translate":
-        console.log("Selected Translate");
-        return;
-      case "Chat":
-        console.log("Selected Chat");
-        return;
-    }
-  });
+const availableActions = [
+  {
+    id: "summarize",
+    title: chrome.i18n.getMessage("cmdSummarize"),
+    action: () => call_to_action("summarize"),
+  },
+  {
+    id: "eli5",
+    title: chrome.i18n.getMessage("cmdELI5"),
+    action: () => call_to_action("eli5"),
+  },
+];
+
+const chatBtn = document.getElementById("action-chat");
+chatBtn.onclick = () => chrome.sidebarAction.open();
+
+if (availableActions?.length) {
+  const content = document.getElementById("popup-content");
+  content.appendChild(document.createElement("hr"));
+  for (const { id, title, action } of availableActions) {
+    const actionButton = document.createElement("button");
+    actionButton.innerText = title;
+    actionButton.setAttribute("id", `action-{id}`);
+    actionButton.onclick = action;
+    content.appendChild(actionButton);
+  }
 }
 
 // localize
 document.querySelectorAll("[data-locale]").forEach((elem) => {
   elem.innerText = chrome.i18n.getMessage(elem.dataset.locale);
 });
-
-listenForClicks();
